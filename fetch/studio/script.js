589ccb65b0ebce0647859013adf9bd6f143def78
Aether1a <olkyrah@outlook.com>
2023-11-09 19:24:15 -0600

    HTML Exercises

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,19 @@
-//TODO: Add Your Code Below
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then( function(response) {
+      response.json().then( function(json) {
+        const div = document.getElementById('container');
+        //loop
+            div.innerHTML = `
+              <ul>
+                <li>id ${json.id}</li>
+                <li>active ${json.active}</li>
+                <li>firstName ${json.firstName}</li>
+                <li>lastName ${json.lastName}</li>
+                <li>skills ${json.skills}</li>
+                <li>hoursInSpace ${json.hoursInSpace}</li>
+                <li>picture ${json.picture}</li>
+              </ul>
+            `;
+          });
+    });
+  });
